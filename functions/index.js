992f88fade7802c0fd8ef7d97815065a7d397ce6
meryem <me.achemlal@gmail.com>
2023-02-28 16:45:47 +0100

    building ...

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-import functions from "firebase-functions";
+const functions = require("firebase-functions");
+
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
